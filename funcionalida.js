36f9a5264ec69605abd6214c62d6c929bba34057
javier52143 <sanchezjavier1985@gmail.com>
2024-07-30 12:23:52 -0500

    first commit

---
+++ funcionalida.js
@@ -0,0 +1,13 @@
+const taskinput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const tasKList = document.getElementById("task-list");
+
+addTaskBtn.addEventListener("click", () => {
+    const taskTex = taskinput.value.trim();
+    if (taskTex.length!==''){
+        const tasKListItem = document.createElement("LI");
+        tasKListtItem.textContent = taskText;
+        tasKLis.appendChild(tasKListItem);
+        taskinput.value = '';
+    }
+});
